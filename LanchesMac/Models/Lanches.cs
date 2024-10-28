d35032bdb92f2dd474f195c8c5057f9e9efeacac
GeronimoOlanda <geronimo.olanda@outlook.com>
2024-10-27 23:26:01 -0300

    Mapeamento para a logica do negocio atraves da model Lanches.

---
+++ LanchesMac/Models/Lanches.cs
@@ -0,0 +1,20 @@
+﻿namespace LanchesMac.Models
+{
+    public class Lanches
+    {
+        public int LancheId { get; set; }
+        public string  Nome { get; set; }
+        public string  DescricaoCurta { get; set; }
+        public string  DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string  ImagemURL { get; set; }
+        public string  ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        //Referencia a Categoria
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
