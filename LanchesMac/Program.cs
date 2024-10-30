6879fd2045e4bfaf230ddd0308bee51e5aa2701c
GeronimoOlanda <geronimo.olanda@outlook.com>
2024-10-29 23:48:50 -0300

    Adicionando string de conexão na program.

--- LanchesMac/Program.cs
+++ LanchesMac/Program.cs
@@ -1,3 +1,6 @@
+using LanchesMac.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace LanchesMac
 {
     public class Program
@@ -8,6 +11,9 @@ namespace LanchesMac
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
             var app = builder.Build();
 
