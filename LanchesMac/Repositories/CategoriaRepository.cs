24764b46aa3d9118294ab39d5164e00375b60c88
GeronimoOlanda <geronimo.olanda@outlook.com>
2024-11-03 00:31:58 -0300

    Iniciando a implementação da repository(padrão de projeto)

---
+++ LanchesMac/Repositories/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+
+namespace LanchesMac.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        //Acessa a tabela categorias e retorna a coleção para a gente.
+        public IEnumerable<Categoria> Categorias => _context.Categorias;
+    }
+}
