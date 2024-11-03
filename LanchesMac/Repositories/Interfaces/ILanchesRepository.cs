c7250ae32f6683e12e4fbe7c954858e2c13ea8bb
GeronimoOlanda <geronimo.olanda@outlook.com>
2024-11-03 00:41:32 -0300

    Criação da interface para a lanchesRepository

---
+++ LanchesMac/Repositories/Interfaces/ILanchesRepository.cs
@@ -0,0 +1,14 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repositories.Interfaces
+{
+    public interface ILanchesRepository 
+    {
+        //somente leitura
+        IEnumerable<Lanches> Lanches { get; }
+
+        IEnumerable<Lanches> LanchesFavoritos { get; }
+
+        Lanches GetLanchesById(int lancheId);
+    }
+}
