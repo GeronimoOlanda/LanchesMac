c7941638212d468cbcefbfa5c04ac0234f0b9cb5
GeronimoOlanda <geronimo.olanda@outlook.com>
2024-11-03 00:32:16 -0300

    Criação da interface da repository.

---
+++ LanchesMac/Repositories/Interfaces/ICategoriaRepository.cs
@@ -0,0 +1,10 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.Repositories.Interfaces
+{
+    public interface ICategoriaRepository
+    {
+        //IEumerable permite somente leitura e iterações simples
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
