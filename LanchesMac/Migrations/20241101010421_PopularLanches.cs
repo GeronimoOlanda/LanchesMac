6515c506d6f5132233a96cfb20ad84537313fda5
GeronimoOlanda <geronimo.olanda@outlook.com>
2024-11-02 21:33:52 -0300

    Populando a tabela lanches.

--- LanchesMac/Migrations/20241101010421_PopularLanches.cs
+++ LanchesMac/Migrations/20241101010421_PopularLanches.cs
@@ -10,13 +10,14 @@ namespace LanchesMac.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId, DescricaoCurta, DescricaoDetalhada, EmEstoque, ImagemThumbnailUrl, ImagemURL, IsLanchePreferido, Nome, Preco) VALUES " +
+                "(1, 'Pão, Hamburguer, ovo, batata palha, salsicha', 'Delicioso pao de hamburguer', 1, 'http://google.com.br', 'http://google.com.br', 0, 'Cheese Salada', 12.50)");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("DELETE FROM Lanches");
         }
     }
 }
