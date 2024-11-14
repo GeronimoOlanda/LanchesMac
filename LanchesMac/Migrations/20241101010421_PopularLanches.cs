3370bd7d323dbec5f1a5ae354c6bd2862367c2e1
GeronimoOlanda <geronimo.olanda@outlook.com>
2024-11-13 23:33:33 -0300

    Atualização da migration e limpeza da visualização de lanche/list

--- LanchesMac/Migrations/20241101010421_PopularLanches.cs
+++ LanchesMac/Migrations/20241101010421_PopularLanches.cs
@@ -11,7 +11,9 @@ namespace LanchesMac.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId, DescricaoCurta, DescricaoDetalhada, EmEstoque, ImagemThumbnailUrl, ImagemURL, IsLanchePreferido, Nome, Preco) VALUES " +
-                "(1, 'Pão, Hamburguer, ovo, batata palha, salsicha', 'Delicioso pao de hamburguer', 1, 'http://google.com.br', 'http://google.com.br', 0, 'Cheese Salada', 12.50)");
+                "(1, 'Pão, Hamburguer, ovo, batata palha, salsicha', 'Delicioso pao de hamburguer', 1, 'https://www.minhareceita.com.br/app/uploads/2021/02/shutterstock_1490753552-scaled.jpg', 'https://www.minhareceita.com.br/app/uploads/2021/02/shutterstock_1490753552-scaled.jpg', 0, 'Cheese Salada', 12.50)");
+            migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId, DescricaoCurta, DescricaoDetalhada, EmEstoque, ImagemThumbnailUrl, ImagemURL, IsLanchePreferido, Nome, Preco) VALUES " +
+               "(2, 'Pão, Hamburguer, ovo, batata palha, salsicha', 'Delicioso pao de hamburguer', 2, 'https://www.minhareceita.com.br/app/uploads/2021/02/shutterstock_1490753552-scaled.jpg', 'https://www.minhareceita.com.br/app/uploads/2021/02/shutterstock_1490753552-scaled.jpg', 0, 'Cheese Salada', 12.50)");
         }
 
         /// <inheritdoc />
