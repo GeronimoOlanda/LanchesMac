9d9409e26ebc0c3841dc52fab6600a33a9b6aa04
GeronimoOlanda <geronimo.olanda@outlook.com>
2024-11-13 23:49:36 -0300

    Entendendo o funcionamento do TempData, ViewBag e VIewData.

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -15,7 +15,17 @@ namespace LanchesMac.Controllers
         // o sistema vai procurar por um cshtml chamado List.cshtml
         public IActionResult List()
         {
+            ViewData["Titulo"] = "Todos os Lanches";
+            ViewData["Data"] = DateTime.Now;
+
             var lanches = _lanchesRepository.Lanches; //acessando lanches
+            var totalLanches = lanches.Count();
+
+
+            //O Nome da variavel como no exemplo abaixo, a gente cria diretamente no viewBag e chama da view.
+            ViewBag.Total = "Total de lanches : ";
+            ViewBag.TotalLanches = totalLanches;
+
             return View(lanches); //retornando para a view os dados.
         }
     }
