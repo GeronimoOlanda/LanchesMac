0ee0c8b28e186ed6294b02e232f735b36866ca37
GeronimoOlanda <geronimo.olanda@outlook.com>
2025-01-09 09:44:35 -0300

    Criando nosso carrinho de compra

---
+++ LanchesMac/Models/CarrinhoCompra.cs
@@ -0,0 +1,20 @@
+﻿using LanchesMac.Context;
+
+namespace LanchesMac.Models
+{
+    public class CarrinhoCompra
+    {
+
+        private readonly AppDbContext _context; 
+
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            this._context = context;
+        }
+
+        public string CarrinhoCOmpraId { get; set; }
+        public List<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
+
+    }
+}
