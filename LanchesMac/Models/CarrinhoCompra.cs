2a2e50cf7c20336eda61aa2b2d2d361c5d6966b2
GeronimoOlanda <geronimo.olanda@outlook.com>
2025-01-09 09:50:06 -0300

    obtem os carrinhos criados pelo cliente na sessão ao acesso ao site

--- LanchesMac/Models/CarrinhoCompra.cs
+++ LanchesMac/Models/CarrinhoCompra.cs
@@ -16,5 +16,28 @@ namespace LanchesMac.Models
         public string CarrinhoCOmpraId { get; set; }
         public List<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
 
+        //obtem os carrinhos criados pelo cliente na sessão ao acesso ao site
+        public static CarrinhoCompra GetCarrinho (IServiceProvider services)
+        {
+            //define uma sessão
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            //obtem um serviço do tipo do nosso contexto
+            AppDbContext context = services.GetService<AppDbContext>();
+
+            //obtem ou gera o id do carrinho
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            //atribui o id do carrinho na sessão
+            session.SetString("CarrinhoId", carrinhoId);
+
+            //retorna o carrinho com o contexto e Id atribuido ou obtido
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCOmpraId = carrinhoId,
+            };
+        
+        }
+
     }
 }
